13ef4b9ef9f64a39fa4b7776906ae8a1153b78c9
SafiaMaani <saf.maani@gmail.com>
2023-01-04 20:11:40 +0100

    installation tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      red: colors.red,
+      'color-primary': '#00aeef',
+      'color-secondary': '#36517e',
+    },
+    fontFamily: {
+      main: ['Roboto', 'sans-serif'],
+      mini: ['Montserrat', 'sans-serif'],
+    }
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: ["light"],
+    base: false,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+}
