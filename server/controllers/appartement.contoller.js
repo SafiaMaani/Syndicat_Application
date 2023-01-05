22b400c6b176f04da01f945876933688d8b55f8c
SafiaMaani <saf.maani@gmail.com>
2023-01-05 16:35:02 +0100

    Create Appartement functions (without implementation)

---
+++ server/controllers/appartement.contoller.js
@@ -0,0 +1,14 @@
+const Appartement = require('../models/appartement.model')
+const Resident = require('../models/resident.model')
+
+const addAppartement = async (req, res) => {}
+const deleteAppartement = async (req, res) => {}
+const updateAppartement = async (req, res) => {}
+const getAllAppartement = async (req, res) => {}
+
+module.exports = {
+  addAppartement,
+  deleteAppartement,
+  updateAppartement,
+  getAllAppartement
+}
