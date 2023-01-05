83a47acdec37a4497db3b8d0b8c9b8fd24670429
SafiaMaani <saf.maani@gmail.com>
2023-01-05 16:00:37 +0100

    Ajout des nested routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-
+import {Routes, Route} from 'react-router-dom'
+import Login from './Pages/Authentification/Login';
+import ForgetPassword from './Pages/Authentification/ForgetPassword';
+import ResetPassword from './Pages/Authentification/ResetPassword';
+import Dashboard from './Pages/Dashboard'
+import Appartements from './Pages/Dashboard/Appartements'
+import Factures from './Pages/Dashboard/Factures'
+import Paiements from './Pages/Dashboard/Paiements'
+import Residents from './Pages/Dashboard/Residents'
 function App() {
   return (
     <div className="App">
-      LOGIN PAGE
+    <Routes>
+      <Route path={'/'} element={<Login />}/>
+      <Route path={'/forgetpassword'} element={<ForgetPassword />}/>
+      <Route path={'/resetpassword/:token'} element={<ResetPassword />}/>
+      <Route path={'/dashboard'} element={<Dashboard />}>
+        <Route path={'appartements'} element={<Appartements />} />
+        <Route path={'factures'} element={<Factures />} />
+        <Route path={'paiements'} element={<Paiements />} />
+        <Route path={'residents'} element={<Residents />} />
+      </Route>
+    </Routes>
+      
     </div>
   );
 }
