6d40fe3e2f613228fa6c3a4a18452c61fe40ccc2
SafiaMaani <saf.maani@gmail.com>
2023-01-06 15:30:37 +0100

    Consommation d'API 'getAllAppartements'

--- client/src/Pages/Dashboard/Appartements.jsx
+++ client/src/Pages/Dashboard/Appartements.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
+import axios from 'axios'
+import Toaster from 'toastr'
+import 'toastr/build/toastr.css'
 
 function Appartements() {
+  const [data, setData] = useState([]);
+
+  const fetchData = ()=>{
+    axios.get("http://localhost:9999/api/appart/getAllAppart")
+    .then((res) =>{
+      setData(res.data.result)
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+  useEffect(()=> {
+    fetchData()
+  },[])
+  
   return (
-    <div>Appartements</div>
+    <div className="overflow-x-auto">
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th>Numéro</th>
+            <th>Chambre</th>
+            <th>Prix</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => 
+            <tr>
+              <td>{item.Number}</td>
+              <td>{item.rooms}</td>
+              <td>{item.price} DH</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>  
   )
 }
 
