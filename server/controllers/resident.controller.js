b601763555df2bb3889c51bce0eed43022286389
SafiaMaani <saf.maani@gmail.com>
2023-01-15 00:37:14 +0100

    Implementation de la méthode getAllResidents

--- server/controllers/resident.controller.js
+++ server/controllers/resident.controller.js
@@ -3,7 +3,17 @@ const Resident = require('../models/resident.model')
 const addResident = async (req, res) => {}
 const deleteResident = async (req, res) => {}
 const updateResident = async (req, res) => {}
-const getAllResident = async (req, res) => {}
+const getAllResident = async (req, res) => {
+  Resident.find().then((result) => {
+    res.status(200).json({
+      result
+    })
+  }).catch((err) => {
+    res.status(400).json({
+      err
+    })
+  })
+}
 
 module.exports = {
   addResident,
