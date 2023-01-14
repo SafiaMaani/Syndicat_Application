83ed8d86f321b36fe92e796c64157377783d70dd
SafiaMaani <saf.maani@gmail.com>
2023-01-15 00:50:17 +0100

    Implementation de la méthode addResident

--- server/controllers/resident.controller.js
+++ server/controllers/resident.controller.js
@@ -1,6 +1,30 @@
 const Resident = require('../models/resident.model')
 
-const addResident = async (req, res) => {}
+const addResident = async (req, res) => {
+
+  const resident = new Resident({
+    fullName: req.body.fullName,
+    cin: req.body.cin,
+    tel: req.body.tel,
+  })
+
+  if (!resident.fullName || !resident.cin || !resident.tel) {
+    return res.status(400).json({
+      message: "Veuillez remplir tous les champs!"
+    })
+  }
+
+  try {
+    await resident.save()
+    return res.status(200).json({
+      message: "Le résident a été ajouté avec succès !"
+    })
+  } catch (error) {
+    return res.status(400).json({
+      message: error
+    })
+  }
+}
 const deleteResident = async (req, res) => {}
 const updateResident = async (req, res) => {}
 const getAllResident = async (req, res) => {
