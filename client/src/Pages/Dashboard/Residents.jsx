784b7e92053e41b1bbc5014110ab782d678263e7
SafiaMaani <saf.maani@gmail.com>
2023-01-15 00:41:32 +0100

    Consommation de la méthode getAllResidents

--- client/src/Pages/Dashboard/Residents.jsx
+++ client/src/Pages/Dashboard/Residents.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
+import axios from 'axios'
 
 function Residents() {
+  const [data, setData] = useState([]);
+
+  const fetchData = ()=>{
+    axios.get("http://localhost:9999/api/residents/getAllResident")
+    .then((res) =>{
+      setData(res.data.result)
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+  useEffect(()=> {
+    fetchData()
+  },[])
   return (
-    <div>Residents</div>
+    <div className="overflow-x-auto">
+    <table className="table w-full">
+      <thead>
+        <tr>
+          <th>Nom complet</th>
+          <th>N° de Carte d'indentité</th>
+          <th>N° de téléphone</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => 
+          <tr>
+            <td>{item.fullName}</td>
+            <td>{item.cin}</td>
+            <td>{item.tel} </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  </div>  
   )
 }
 
