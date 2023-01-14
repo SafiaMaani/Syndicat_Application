1fa2084579030056cafaf0e13cb5933c0c2921e4
SafiaMaani <saf.maani@gmail.com>
2023-01-15 00:36:20 +0100

    Creations des méthodes de Résident

---
+++ server/controllers/resident.controller.js
@@ -0,0 +1,13 @@
+const Resident = require('../models/resident.model')
+
+const addResident = async (req, res) => {}
+const deleteResident = async (req, res) => {}
+const updateResident = async (req, res) => {}
+const getAllResident = async (req, res) => {}
+
+module.exports = {
+  addResident,
+  deleteResident,
+  updateResident,
+  getAllResident
+}
