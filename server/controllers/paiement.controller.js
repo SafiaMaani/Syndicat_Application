d85a54acbc62bbf1ce575b81f091143fb533606a
SafiaMaani <saf.maani@gmail.com>
2023-01-15 14:47:54 +0100

    Implementation de la méthode addPaiment

--- server/controllers/paiement.controller.js
+++ server/controllers/paiement.controller.js
@@ -11,7 +11,31 @@ const getAllPaiement = async (req, res) => {
     })
   })
 }
-const addPaiement = async (req, res) => {}
+const addPaiement = async (req, res) => {
+  const paiement = new Paiement({
+    statut: req.body.statut,
+    id_resid: req.body.id_resid,
+    id_appart: req.body.id_appart,
+    mois: req.body.mois,
+  })
+
+  if (!paiement.statut || !paiement.id_resid || !paiement.id_appart || !paiement.mois) {
+    return res.status(400).json({
+      message: "Veuillez remplir tous les champs!"
+    })
+  }
+
+  try {
+    await paiement.save()
+    return res.status(200).json({
+      message: "Le paiement a été ajouté avec succès !"
+    })
+  } catch (error) {
+    return res.status(400).json({
+      message: error
+    })
+  }
+}
 const updatePaiement = (req, res) => {}
 
 module.exports = {
