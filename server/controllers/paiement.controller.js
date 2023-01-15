076eaa33bd9d1b590af01991fc03f18e23dcfbaa
SafiaMaani <saf.maani@gmail.com>
2023-01-15 14:38:41 +0100

    Implementation de la methode getAllPaiement

--- server/controllers/paiement.controller.js
+++ server/controllers/paiement.controller.js
@@ -1,6 +1,16 @@
 const Paiement = require('../models/paiement.model')
 
-const getAllPaiement = async (req, res) => {}
+const getAllPaiement = async (req, res) => {
+  Paiement.find().then((result) => {
+    res.status(200).json({
+      result
+    })
+  }).catch((err) => {
+    res.status(400).json({
+      err
+    })
+  })
+}
 const addPaiement = async (req, res) => {}
 const updatePaiement = (req, res) => {}
 
