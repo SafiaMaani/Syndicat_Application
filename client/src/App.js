6a2f0c1f4d07a993a45c449e38547de90345c3b5
SafiaMaani <saf.maani@gmail.com>
2023-01-15 20:48:34 +0100

    Ajout component protection du dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,24 @@ import Appartements from './Pages/Dashboard/Appartements'
 import Factures from './Pages/Dashboard/Factures'
 import Paiements from './Pages/Dashboard/Paiements'
 import Residents from './Pages/Dashboard/Residents'
+import ProtectDashboard from './Utils/protectDashboard';
+
 function App() {
   return (
     <div className="App">
     <Routes>
+      {/* Public Routes */}
       <Route path={'/'} element={<Login />}/>
       <Route path={'/forgetpassword'} element={<ForgetPassword />}/>
       <Route path={'/resetpassword/:token'} element={<ResetPassword />}/>
-      <Route path={'/dashboard'} element={<Dashboard />}>
-        <Route path={'appartements'} element={<Appartements />} />
-        <Route path={'factures'} element={<Factures />} />
-        <Route path={'paiements'} element={<Paiements />} />
-        <Route path={'residents'} element={<Residents />} />
+      {/* Protected Routes */}
+      <Route element={<ProtectDashboard />}>
+        <Route path={'/dashboard'} element={<Dashboard />}>
+          <Route path={'appartements'} element={<Appartements />} />
+          <Route path={'factures'} element={<Factures />} />
+          <Route path={'paiements'} element={<Paiements />} />
+          <Route path={'residents'} element={<Residents />} />
+        </Route>
       </Route>
     </Routes>
       
