dcf023badbe5dbddee47dc1917d61ed0fbea145a
SafiaMaani <saf.maani@gmail.com>
2023-01-15 14:37:51 +0100

    Creation des methodes paiments

---
+++ server/controllers/paiement.controller.js
@@ -0,0 +1,11 @@
+const Paiement = require('../models/paiement.model')
+
+const getAllPaiement = async (req, res) => {}
+const addPaiement = async (req, res) => {}
+const updatePaiement = (req, res) => {}
+
+module.exports = {
+  addPaiement,
+  updatePaiement,
+  getAllPaiement
+}
