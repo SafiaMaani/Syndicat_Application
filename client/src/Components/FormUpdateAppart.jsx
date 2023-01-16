ebee7e6a2c2059ad4118fd768c25f2444fa39810
SafiaMaani <saf.maani@gmail.com>
2023-01-16 11:29:12 +0100

    Update

--- client/src/Components/FormUpdateAppart.jsx
+++ client/src/Components/FormUpdateAppart.jsx
@@ -1,11 +1,12 @@
 import {useState, useEffect} from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
+import ImageUpdate from '../Assets/images/update.svg'
 import axios from 'axios'
 
 function FormUpdateAppart() {
 
   const navigate = useNavigate()
-  
+
   const {id} = useParams()
   const [data, setData] = useState({})
   
@@ -27,8 +28,8 @@ function FormUpdateAppart() {
   }
 
   return (
-    <div>
-      <form className="form-control" onSubmit={handleSubmit}>
+    <div className='flex'>
+      <form className="form-control w-1/2" onSubmit={handleSubmit}>
         <input value={data?.fullName} onChange={handleChange} name='fullName' type="text" placeholder="Nom complet" className="input input-bordered my-4" />
         <input value={data?.cin} onChange={handleChange} name='cin' type="text" placeholder="N° DE CARTE D'INDENTITÉ" className="input input-bordered my-4" />
         <input value={data?.tel} onChange={handleChange} name='tel' type="text" placeholder="N° DE TÉLÉPHONE" className="input input-bordered my-4" />
@@ -36,6 +37,9 @@ function FormUpdateAppart() {
         <button type='submit' className="btn btn-primary">Update</button>
       </div>
       </form>
+      <div className='w-1/2 h-1/2'>
+        <img src={ImageUpdate} alt='update'/>
+      </div>
     </div>
   )
 }
